f19522e3dbad375db5b2fee4e0607651dd4a8e88
Sumanth Manjunath <sumanthm@adobe.com>
2024-11-19 09:55:08 +0530

    Demo Changes

--- blocks/info-block/info-block.js
+++ blocks/info-block/info-block.js
@@ -1,18 +1,18 @@
 export default function decorate(block) {
-  const title = block.querySelector('h2').textContent;
-  const description = block.querySelector('p').textContent;
-  const pictureSources = [...block.querySelectorAll('source')];
-  const imgSrc = block.querySelector('img').src;
+  const title = block.querySelector("h2").textContent;
+  const description = block.querySelector("p").textContent;
+  const pictureSources = [...block.querySelectorAll("source")];
+  const imgSrc = block.querySelector("img").src;
 
   block.innerHTML = `
     <div class="people-progress-prosperity-wrapper">
       <div class="text-content">
-        <h1>${title}</h1>
+        <h2>${title}</h1>
         <p>${description}</p>
       </div>
       <div class="image-content">
         <picture>
-          ${pictureSources.map(source => source.outerHTML).join('')}
+          ${pictureSources.map((source) => source.outerHTML).join("")}
           <img src="${imgSrc}" alt="">
         </picture>
       </div>
@@ -20,17 +20,19 @@ export default function decorate(block) {
   `;
 
   // Add scroll event listener to trigger animations
-  window.addEventListener('scroll', () => {
-    const wrapper = document.querySelector('.people-progress-prosperity-wrapper');
-    const textContent = wrapper.querySelector('.text-content');
-    const imageContent = wrapper.querySelector('.image-content');
+  window.addEventListener("scroll", () => {
+    const wrapper = document.querySelector(
+      ".people-progress-prosperity-wrapper"
+    );
+    const textContent = wrapper.querySelector(".text-content");
+    const imageContent = wrapper.querySelector(".image-content");
     const wrapperPosition = wrapper.getBoundingClientRect().top;
     const screenPosition = window.innerHeight / 1.3;
 
     if (wrapperPosition < screenPosition) {
-      wrapper.classList.add('animate__animated', 'animate__fadeIn');
-      textContent.classList.add('animate__animated', 'animate__fadeInLeft');
-      imageContent.classList.add('animate__animated', 'animate__fadeInRight');
+      wrapper.classList.add("animate__animated", "animate__fadeIn");
+      textContent.classList.add("animate__animated", "animate__fadeInLeft");
+      imageContent.classList.add("animate__animated", "animate__fadeInRight");
     }
   });
 }
